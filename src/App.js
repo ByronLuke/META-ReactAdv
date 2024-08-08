a69c7e582df256db31ecf42ed187a97c55318d7f
ByronLuke <byronpc18@icloud.com>
2024-08-08 16:17:42 -0500

    useState hook

--- src/App.js
+++ src/App.js
@@ -1,99 +1,47 @@
-import "./App.css";
 import { useState } from "react";
-import { validateEmail } from "./utils";
 
-const PasswordErrorMessage = (e) => {
-  return (
-    <p className="FieldError">Password should have at least 8 characters</p>
+export default function App() {
+  const [giftCard, setGiftCard] = useState(
+    {
+        firstName: "Jennifer",
+        lastName: "Smith",
+        text: "Free dinner for 4 guests",
+        valid: true,
+        instructions: "To use your coupon, click the button below.",
+    }
   );
-};
 
-function App() {
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState({
-    value: "",
-    isTouched: false,
-  });
-  const [role, setRole] = useState("role");
-
-  const getIsFormValid = () => {
-    // Implement this function
-    return (
-      firstName && lastName && validateEmail(email) && password.value >=8 && role !== "role"
-    );
-  };
-
-  const clearForm = () => {
-    setFirstName("")
-    setLastName("")
-    setEmail("")
-    setPassword(
-      {
-        value: "",
-        isTouched: false
-      }
-    )
-    setRole("role")
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    alert("Account created!");
-    clearForm();
-  };
+  function spendGiftCard() {
+    setGiftCard((prevState)=>{
+      const newState = {...prevState}
+      newState.text = "Your coupon has been used."
+      newState.valid = false;
+      newState.instructions = "Please visit our restaurant to renew your gift card.."
+      return newState;
+    })
+  }
 
   return (
-    <div className="App">
-      <form onSubmit={handleSubmit}>
-        <fieldset>
-          <h2>Sign Up</h2>
-          <div className="Field">
-            <label>
-              First name <sup>*</sup>
-            </label>
-            <input placeholder="First name" value={firstName} onChange={ (e)=>setFirstName(e.target.value)}/>
-          </div>
-          <div className="Field">
-            <label>Last name</label>
-            <input placeholder="Last name" value={lastName} onChange={(e)=>setLastName(e.target.value)}/>
-          </div>
-          <div className="Field">
-            <label>
-              Email address <sup>*</sup>
-            </label>
-            <input placeholder="Email address" value={email} onChange={(e)=>setEmail(e.target.value)}/>
-          </div>
-          <div className="Field">
-            <label>
-              Password <sup>*</sup>
-            </label>
-            <input placeholder="Password" 
-            value={password.value} 
-            onChange={(e)=>setPassword({...password, value: e.target.value})}  
-            onBlur={()=> setPassword({...password, isTouched: true})} />
-            {password.isTouched && password.value.length < 8 ? (
-              <PasswordErrorMessage></PasswordErrorMessage>
-            ) : null}
-          </div>
-          <div className="Field">
-            <label>
-              Role <sup>*</sup>
-            </label>
-            <select onChange={(e)=>setRole(e.target.value)} value={role}>
-              <option value="role" >Role</option>
-              <option value="individual" >Individual</option>
-              <option value="business"> Business</option>
-            </select>
-          </div>
-          <button type="submit" disabled={!getIsFormValid()}>
-            Create account
+    <div style={{padding: '40px'}}>
+      <h1>
+        Gift Card Page
+      </h1>
+      <h2>
+        Customer: {giftCard.firstName} {giftCard.lastName}
+      </h2>
+      <h3>
+        {giftCard.text}
+      </h3>
+      <p>
+        {giftCard.instructions}
+      </p>
+      {
+        giftCard.valid && (
+          <button onClick={spendGiftCard}>
+            Spend Gift Card
           </button>
-        </fieldset>
-      </form>
+        )
+      }
     </div>
   );
 }
-
-export default App;
